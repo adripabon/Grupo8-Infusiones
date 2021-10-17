c06574be3bada4d53b8f6f37c305861a6b932753
juantautiva <juantautiva@gmail.com>
2021-10-16 23:07:11 -0500

    Se adiciona la navegación al login y se matricula en app.js

--- app.js
+++ app.js
@@ -9,5 +9,9 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/index.html'));
 })
 
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'));
+})
+
 app.listen(PUERTO, () => console.log(`Servidor escuchando por el puerto ${PUERTO}`)
 );
