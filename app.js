c36ab18d9cd5c777f4537ba39545d377b8a62e5b
Joaquin Cernik <joaquincernik@gmail.com>
2021-10-20 22:40:25 -0300

    Actualizacion

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/index.html'));
-})
+});
+
+app.get('/login',(req, res)=>{
+    res.sendFile(path.join(__dirname, "./views/login.html"))
+});
 
 app.listen(PUERTO, () => console.log(`Servidor escuchando por el puerto ${PUERTO}`)
 );
