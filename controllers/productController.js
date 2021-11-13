4fa089cf37e356ade01d338c36f9b8cb33a57df2
juantautiva <juantautiva@gmail.com>
2021-11-13 12:07:52 -0500

    Se termina con el sprint 4, se sube el delete y el crear.

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,47 +1,74 @@
-const jsonTable = require('../data/jsonTable')
-const productsModel = jsonTable('products')
+const jsonTable = require("../data/jsonTable");
+const productsModel = jsonTable("products");
 
 /* Contine los controladores del index */
 const productController = {
-    /*  index: (req,res)=>{
+  /*  index: (req,res)=>{
          res.render('index',{about, 'menu': listaPlatos})
      }, */
-    myProducts: (req, res) => {
-        res.render('products/myProducts');
-    },
-    productCart: (req, res) => {
-        res.render('products/productCart');
-    },
-    productDetails: (req, res) => {
-        let id = req.params.id
+  myProducts: (req, res) => {
+    res.render("products/myProducts");
+  },
+  productCart: (req, res) => {
+    res.render("products/productCart");
+  },
+  productDetails: (req, res) => {
+    let id = req.params.id;
 
-        const product = productsModel.find(id)
+    const product = productsModel.find(id);
 
-        res.render('products/productDetails', { product });
-    },
-    edit: (req,res)=> {
-        let id= req.params.id;
-        const product = productsModel.find(id);
-        res.render("products/edit-myProducts", {product});
-   },
+    res.render("products/productDetails", { product });
+  },
+  edit: (req, res) => {
+    let id = req.params.id;
+    const product = productsModel.find(id);
+    res.render("products/edit-myProducts", { product });
+  },
 
-   update: (req,res)=>{
-    let id= req.params.id;
+  update: (req, res) => {
+    let id = req.params.id;
     let product = productsModel.find(id);
-    console.log(req.file);
-    if(req.file)
-            image = req.file.filename
-        else
-            image = product.image
+    //console.log(req.file);
+    if (req.file)
+        image = req.file.filename;
+    else 
+        image = product.image;
+
     product = {
-        id: product.id,
+      id: product.id,
+      ...req.body,
+      image: image,
+    };
+
+    id = productsModel.update(product);
+
+    res.render("products/productDetails", { product });
+  },
+  create: (req,res) => {
+    let id = productsModel.nextId()
+    let image = ''
+
+    if (req.file)
+        image = req.file.filename;
+
+    let product = {
+        id:id,
         ...req.body,
-        image: image
+        image:image
     }
-    res.send("archivos enviados")
-},
 
+    id = productsModel.create(product);
 
- }
- 
- module.exports = productController
+    res.render("products/productDetails", { product });
+
+  },
+  delete: ( req, res ) => {
+      let id = req.params.id
+      productsModel.delete(id)
+      
+      res.redirect('/')
+  }
+
+};
+
+module.exports = productController;
--- routers/productRouters.js
+++ routers/productRouters.js
@@ -1,17 +1,15 @@
 const { application } = require('express');
 const express = require('express')
 const router = express.Router()
-const multer= require('multer')
+const multer = require('multer')
 const path = require('path')
 
 
 const storage = multer.diskStorage({
     destination: (req,file,cb) =>{
-        console.log("aqui");
         cb(null, path.join(__dirname, '../public/images'))
     },
     filename: (req,file,cb) =>{
-       console.log(file.originalName);
         const newFileName = 'PRODUCTS-' + Date.now() + path.extname(file.originalname)
         cb(null, newFileName)
     } 
@@ -21,14 +19,20 @@ const upload= multer({storage:storage})
 const productController = require('../controllers/productController')
 
 router.get("/myProducts", productController.myProducts);
-
 router.get("/product-cart", productController.productCart);
 
+/* CREATE PRODUCT */
+router.post("/create",upload.single('image'), productController.create )
+
+/* GET PRODUCT */
 router.get("/product-details/:id", productController.productDetails);
 
+/* EDIT PRODUCT */
 router.get("/edit/:id", productController.edit)
-
 router.put("/edit/:id",upload.single('image'), productController.update )
+
+/* DELET PRODUCT */
+router.delete("/delete/:id", productController.delete )
 
 
 module.exports = router
