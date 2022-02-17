8f2b735ec7c526d326605d54280df3c7bbfa272b
juantautiva <juantautiva@gmail.com>
2022-02-16 21:17:46 -0500

    Ejecución de Dashboard y ajuste de APIS

--- controllers/apiControllers/apiProductsController.js
+++ controllers/apiControllers/apiProductsController.js
@@ -44,27 +44,27 @@ const apiProductsController={
     list:(req, res) =>{
    
     db.Products.findAll({
-        attributes:['id_products','description', 'price', 'discount', 'id_category_product', 'id_type_product', 'image', [db.sequelize.fn('CONCAT', '/api/product/',  db.Sequelize.col('id_products')), 'url_detail'] ],
+        attributes:['id_products','name','description', 'price', 'discount', 'id_category_product', 'id_type_product', 'image', [db.sequelize.fn('CONCAT', '/api/product/',  db.Sequelize.col('id_products')), 'url_detail'] ],
         include: [
             { association: "typeProduct" },
             { association: "categoryProducts" },
           ]
     })
      .then(products=>{
-        let productsArray = [];
-        let productObj 
-        products.map( product => {
+        /* const productsArray =  products.map( product => {
             
             let url_detail = `/api/product/${product.id_products}` 
-            productObj = { product,  url_detail: url_detail }
-            productsArray.push(productObj)
-        })
+            product.url_detail = url_detail
+            
+            //const productObj = { ...product,  url_detail }
+            return product
+        }) */
 
         //console.log(userArray)
          let resultado = {
              count : {count:products.length},
              countByCategory : categoriasArray[0].countByCategory,
-             products : productsArray
+             products : products
          }
          res.json(resultado)
         }) 
@@ -72,12 +72,21 @@ const apiProductsController={
     },
 
     detail:(req,res)=>{
-        db.Products.findByPk(req.params.id,)
+        db.Products.findAll({
+            attributes:['id_products','name','description', 'price', 'discount', 'id_category_product', 'id_type_product', [db.sequelize.fn('CONCAT', 'http://localhost:3000/images/',  db.Sequelize.col('image')), 'image'] ],
+            include: [
+                { association: "typeProduct" },
+                { association: "categoryProducts" },
+            ],
+            where: {
+                id_products: Number(req.params.id)
+            }
+        })
         .then(product=>{
             
             let resultado = {
-            product: product,
-            url_image : `localhost:3000/images/AVATAR/${product.image}`
+                product: product,
+                //url_image : `localhost:3000/images/AVATAR/${product.image}`
             }
             res.json(resultado)
         }) 
